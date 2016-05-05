1062e25bf6482c3ca056125ba794a3995b3f7153
Gordon Leacock <gordonl@umich.edu>
2016-05-05 14:57:18 -0400

    add presenter documentation

---
+++ app/presenters/curation_concerns/presenters.rb
@@ -0,0 +1,8 @@
+module CurationConcerns
+	# This will be the module for containing presenters
+	# @since 0.14.0
+	# Presenters are used to organize and show application data to users. Typically, presenter data structures are
+	# instantiated in the controller to show the data from one object.
+	module Presenters
+	end
+end
